3263bad0a51197482bc5ba520e7ada2cbaf33068
bhosalesuraj074 <125651189+bhosalesuraj074@users.noreply.github.com>
2023-08-09 18:29:41 +0530

    3 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Route, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ObservablesComponent } from './observables/observables.component';
+import { PromiseComponent } from './promise/promise.component';
+
+const routes: Route[] = [
+  { path: '', redirectTo: 'promises', pathMatch: 'full' },
+  { path: 'promises', component: PromiseComponent },
+  { path: 'observables', component: ObservablesComponent },
+];
 
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  declarations: [AppComponent, PromiseComponent],
   bootstrap: [AppComponent],
+  exports: [RouterModule],
 })
 export class AppModule {}
---
+++ src/app/promise/promise.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-promise',
+  templateUrl: './promise.component.html',
+  styleUrls: ['./promise.component.css'],
+})
+export class PromiseComponent implements OnInit {
+  constructor() {}
+  data: any;
+  ngOnInit() {}
+  fetchData() {
+    this.data = 'Fetching data...';
+    return setTimeout(() => {
+      fetch('https://jsonplaceholder.typicode.com/users')
+        .then((res) => res.json())
+        .then((res) => (this.data = JSON.stringify(res)))
+        .catch((res) => (this.data = 'failed to load data try again'));
+    }, 3000);
+    console.log('hello');
+  }
+}
