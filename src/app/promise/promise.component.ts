aed1d51e7b354ab4c0a970405ee2b649a65d3a8d
bhosalesuraj074 <125651189+bhosalesuraj074@users.noreply.github.com>
2023-08-10 10:51:05 +0530

    2 files modified, 1 added

--- src/app/promise/promise.component.ts
+++ src/app/promise/promise.component.ts
@@ -8,16 +8,67 @@ import { Component, OnInit } from '@angular/core';
 export class PromiseComponent implements OnInit {
   constructor() {}
   data: any;
+  asyncData: any;
   ngOnInit() {}
+
+  dell = {
+    brand: 'dell',
+    status: false,
+    color: 'black',
+  };
+  hp = {
+    brand: 'hp',
+    status: false,
+    color: 'silver',
+  };
+  notAvailable = {
+    brand: '',
+    status: 'Laptop not avilable',
+  };
+  promiseData() {
+    let promise = new Promise((resolve, reject) => {
+      if (this.dell.status) {
+        resolve(this.dell);
+      } else if (this.hp.status) {
+        resolve(this.hp);
+      } else {
+        reject(this.notAvailable);
+      }
+    });
+    return promise;
+  }
   fetchData() {
     this.data = 'Fetching data...';
-    let res = setTimeout(() => {
-      fetch('https://jsonplaceholder.typicode.com/users')
-        .then((res) => res.json())
-        .then((res) => (this.data = JSON.stringify(res)))
-        .catch((res) => (this.data = 'failed to load data try again'));
+    // let res = setTimeout(() => {
+    //   fetch('https://jsonplaceholder.typicode.com/users')
+    //     .then((res) => res.json())
+    //     .then((res) => (this.data = JSON.stringify(res)))
+    //     .catch((res) => (this.data = 'failed to load data try again'));
+    // }, 3000);
+    // console.log('hello ji');
+    // //return res;
+    setTimeout(() => {
+      let a = this.promiseData();
+      a.then((res) => {
+        console.log('Then Data', res);
+      });
+      a.catch((res) => {
+        console.log('catch data', res);
+      });
     }, 3000);
-    console.log('hello ji');
-    return res;
+  }
+
+  async getAsyncData() {
+    this.asyncData = 'Fatching data...';
+    let res = setTimeout(() => {
+      fetch('https://jsonplaceholder.typicode.com/todos/1001')
+        .then((res) => res.json())
+        .then((res) => (this.asyncData = JSON.stringify(res)))
+        .catch(
+          (res) =>
+            (this.asyncData =
+              'requested data is not available : ' + JSON.stringify(res))
+        );
+    }, 3000);
   }
 }
