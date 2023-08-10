5190bb5ad47a4ec3a6bc292465ea61bfe29185c0
bhosalesuraj074 <125651189+bhosalesuraj074@users.noreply.github.com>
2023-08-10 11:35:43 +0530

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Route, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { ObservablesComponent } from './observables/observables.component';
 import { PromiseComponent } from './promise/promise.component';
 
 const routes: Route[] = [
-  { path: '', redirectTo: 'promises', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'promises', component: PromiseComponent },
   { path: 'observables', component: ObservablesComponent },
+  { path: '**', redirectTo: 'home' },
 ];
 
 @NgModule({
   imports: [BrowserModule, RouterModule.forRoot(routes)],
-  declarations: [AppComponent, PromiseComponent],
+  declarations: [AppComponent, PromiseComponent, HomeComponent],
   bootstrap: [AppComponent],
   exports: [RouterModule],
 })
