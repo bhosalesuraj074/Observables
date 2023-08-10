e647d8ffb2b83d78d3b9a7b1bbc92eda90fc6570
bhosalesuraj074 <125651189+bhosalesuraj074@users.noreply.github.com>
2023-08-10 12:32:15 +0530

    2 files modified

--- src/app/observables/observables.component.ts
+++ src/app/observables/observables.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-observables',
   templateUrl: './observables.component.html',
-  styleUrls: ['./observables.component.css']
+  styleUrls: ['./observables.component.css'],
 })
 export class ObservablesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor() {}
+  numberArr = [14, 9, 8, 15, 3, 67];
+  ngOnInit() {}
 }
