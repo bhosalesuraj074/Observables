a914c8331dd0fd5e0bf3288f6289b57137bc8fce
bhosalesuraj074 <125651189+bhosalesuraj074@users.noreply.github.com>
2023-08-11 15:08:05 +0530

    3 files modified, 1 added

--- src/app/observables/creation/creation.component.ts
+++ src/app/observables/creation/creation.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-creation',
   templateUrl: './creation.component.html',
-  styleUrls: ['./creation.component.css']
+  styleUrls: ['./creation.component.css'],
 })
 export class CreationComponent implements OnInit {
+  arr = [1, 2, 3, 345];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  ofOperator() {
+    console.log('---- Creation Operator: of ----');
+    let observable = of(this.arr);
+    observable.subscribe((res) => {
+      console.log('of operator res =>', res);
+    });
   }
-
 }
--- src/app/observables/observables.component.ts
+++ src/app/observables/observables.component.ts
@@ -7,19 +7,13 @@ import { Observable, of, Subscription } from 'rxjs';
   styleUrls: ['./observables.component.css'],
 })
 export class ObservablesComponent implements OnInit, OnDestroy {
-  arr = [1, 2, 3, 345];
+ 
   constructor() {}
 
   ngOnInit() {}
 
   // creation operator : of operator
-  ofOperator() {
-    console.log('---- Creation Operator: of ----');
-    let observable = of(this.arr);
-    observable.subscribe((res) => {
-      console.log('of operator res =>', res);
-    });
-  }
+  
 
   ngOnDestroy(): void {}
 }
