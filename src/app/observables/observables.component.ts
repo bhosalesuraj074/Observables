a0b9cae9d11d8b6054296bff8c6ffd34048ceb54
bhosalesuraj074 <125651189+bhosalesuraj074@users.noreply.github.com>
2023-08-11 16:00:13 +0530

    2 files modified

--- src/app/observables/observables.component.ts
+++ src/app/observables/observables.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable, of, Subscription } from 'rxjs';
 
 @Component({
@@ -8,7 +8,7 @@ import { Observable, of, Subscription } from 'rxjs';
   styleUrls: ['./observables.component.css'],
 })
 export class ObservablesComponent implements OnInit, OnDestroy {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit() {}
 
@@ -16,7 +16,7 @@ export class ObservablesComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {}
 
-  callOf() {
-    this.router.navigateByUrl('creation');
+  callOf(call: string) {
+    this.router.navigate([call], { relativeTo: this.route });
   }
 }
