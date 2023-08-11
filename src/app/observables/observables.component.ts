b3f1a1d4f54684ec19b6da4320815eacc640f029
bhosalesuraj074 <125651189+bhosalesuraj074@users.noreply.github.com>
2023-08-11 12:17:05 +0530

    1 file modified

--- src/app/observables/observables.component.ts
+++ src/app/observables/observables.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-observables',
   templateUrl: './observables.component.html',
   styleUrls: ['./observables.component.css'],
 })
-export class ObservablesComponent implements OnInit {
+export class ObservablesComponent implements OnInit, OnDestroy {
+  myObj: Observable<string> | undefined;
+
   constructor() {}
- 
+
   ngOnInit() {
-    //this.
+    this.myObj = new Observable((obserever) => {
+      obserever.next('Suraj');
+      obserever.next('Akash');
+      // this may breake the streaming the data
+      // .'. obserever.error('hello');
+
+      obserever.next('Shubham');
+
+      obserever.complete();
+    });
+
+    // Subscribing the obervables
+    this.myObj.subscribe(
+      (res) => {
+        console.log(res);
+      },
+      (error) => {
+        console.log('Error => ', error);
+      },
+      () => {
+        console.log('Stream is complete');
+      }
+    );
   }
 
+
+  ngOnDestroy(): void {
+   
+    }
 }
