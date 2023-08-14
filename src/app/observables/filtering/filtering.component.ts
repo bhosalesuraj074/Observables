345b070479c637f355a087b5f5e417e975fdd054
bhosalesuraj074 <125651189+bhosalesuraj074@users.noreply.github.com>
2023-08-14 12:42:39 +0530

    2 files modified

--- src/app/observables/filtering/filtering.component.ts
+++ src/app/observables/filtering/filtering.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { of } from 'rxjs';
+import { interval, of, takeWhile } from 'rxjs';
 import { debounceTime, take } from 'rxjs/operators';
 
 @Component({
@@ -20,8 +20,33 @@ export class FilteringComponent implements OnInit {
 
   // debounceTimeOperator
   debounceTimeOperator() {
+    console.log('---- filtering Operator: debounceTime ----');
     this.myForm.controls['search'].valueChanges
       .pipe(debounceTime(3000), take(2))
+      .subscribe((res) => {
+        console.log(res);
+      });
+  }
+  // 2. take operator
+  takeOperator() {
+    console.log('---- filtering Operator: take ----');
+    let intervalOperator = interval(1000);
+    let subscription = intervalOperator.pipe(take(2)).subscribe((res) => {
+      if (res >= 5) {
+        subscription.unsubscribe();
+        console.log('hello ji');
+      } else {
+        console.log(res);
+      }
+    });
+  }
+
+  // takeWhile operartor
+  takeWhileOperator() {
+    console.log('---- filtering Operator: takeWhile ----');
+    let intervalOperator = interval(1000);
+    let takeWhileObservalbe = intervalOperator
+      .pipe(takeWhile((value) => value < 5))
       .subscribe((res) => {
         console.log(res);
       });
