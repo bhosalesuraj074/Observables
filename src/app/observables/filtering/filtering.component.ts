013b5429219fa371f761b8e175ba23c2100927e3
bhosalesuraj074 <125651189+bhosalesuraj074@users.noreply.github.com>
2023-08-14 16:08:09 +0530

    4 files modified

--- src/app/observables/filtering/filtering.component.ts
+++ src/app/observables/filtering/filtering.component.ts
@@ -1,7 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { interval, of, takeWhile } from 'rxjs';
-import { debounceTime, take } from 'rxjs/operators';
+import { FormControl, FormGroup } from '@angular/forms';
+import { interval, takeWhile, takeLast, from, skip } from 'rxjs';
+import {
+  debounceTime,
+  distinct,
+  elementAt,
+  filter,
+  first,
+  last,
+  take,
+} from 'rxjs/operators';
 
 @Component({
   selector: 'app-filtering',
@@ -20,9 +28,9 @@ export class FilteringComponent implements OnInit {
 
   // debounceTimeOperator
   debounceTimeOperator() {
-    console.log('---- filtering Operator: debounceTime ----');
+    //console.log('---- filtering Operator: debounceTime ----');
     this.myForm.controls['search'].valueChanges
-      .pipe(debounceTime(3000), take(2))
+      .pipe(debounceTime(3000))
       .subscribe((res) => {
         console.log(res);
       });
@@ -41,14 +49,78 @@ export class FilteringComponent implements OnInit {
     });
   }
 
-  // takeWhile operartor
+  //3 . takeWhile operartor
   takeWhileOperator() {
     console.log('---- filtering Operator: takeWhile ----');
     let intervalOperator = interval(1000);
-    let takeWhileObservalbe = intervalOperator
-      .pipe(takeWhile((value) => value < 5))
-      .subscribe((res) => {
-        console.log(res);
-      });
+    intervalOperator.pipe(takeWhile((value) => value < 5)).subscribe((res) => {
+      console.log(res);
+    });
+  }
+
+  // 4. takeLast operator
+  takeLastOperator() {
+    console.log('---- filtering Operator: takeLast ----');
+    let observable = from([12, 42, 345, 534, 35, 3]);
+    observable.pipe(takeLast(2)).subscribe((res) => {
+      console.log(res);
+    });
+  }
+
+  // 5.  operator
+  firstOperator() {
+    console.log('---- filtering Operator: first ----');
+    let observable = from([13, 23, 4, 342, 234, 233]);
+    observable.pipe(first((value) => value % 2 == 0)).subscribe((res) => {
+      console.log(res);
+    });
+  }
+
+  // 6. takeLastOperator operator
+  lastOperator() {
+    console.log('---- filtering Operator: last ----');
+    let observable = from([13, 23, 4, 342, 234, 234]);
+    observable.pipe(last((value) => value % 2 == 0)).subscribe((res) => {
+      console.log(res);
+    });
+  }
+  //7. elementAt Operator
+  elementAtOperator() {
+    console.log('---- filtering Operator: elementAt ----');
+    let observable = from(['suraj', 'akash', 'shubham']);
+    observable.pipe(elementAt(0)).subscribe((res) => {
+      console.log(res);
+    });
+  }
+
+  // 8. filter Operator
+  filterOperator() {
+    console.log('---- filtering Operator: filter ----');
+    let observable = from([23, 423, 523, 3, 34, 52, 34]);
+    observable.pipe(filter((value) => value % 2 != 0)).subscribe((res) => {
+      console.log(res);
+    });
+  }
+
+  // 9. distinct Operator
+  distinctOperator() {
+    console.log('---- filtering Operator: distinct ----');
+    let arr = [23, 2, 234, 4, 23, 2];
+    let observable = from(arr);
+    console.log('before filter', arr);
+    observable.pipe(distinct()).subscribe((res) => {
+      console.log(res);
+    });
+  }
+
+  // 10. skip Operator
+  skipOperator() {
+    console.log('---- filtering Operator: skip ----');
+    let arr = ['suraj', 'shubham', 'akash', 'kumar', 'Amruta'];
+    let observable = from(arr);
+    console.log('actual array', arr);
+    observable.pipe(skip(2)).subscribe((res) => {
+      console.log(res);
+    });
   }
 }
