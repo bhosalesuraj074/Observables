e2ed0c1c153c41e70bc807705bd7a1f9d18a9f67
bhosalesuraj074 <125651189+bhosalesuraj074@users.noreply.github.com>
2023-08-16 16:08:43 +0530

    3 files modified

--- src/app/observables/transformation/transformation.component.ts
+++ src/app/observables/transformation/transformation.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { from, of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { from } from 'rxjs';
+import { map, mapTo, mergeMap } from 'rxjs/operators';
 @Component({
   selector: 'app-transformation',
   templateUrl: './transformation.component.html',
@@ -13,13 +13,22 @@ export class TransformationComponent implements OnInit {
 
   mapOperator() {
     console.log('---- transformation Operator: map ----');
+    console.log('Actual element', this.arr);
     let observable = from(this.arr);
     observable.pipe(map((value) => value * 2)).subscribe((res) => {
       console.log(res);
     });
   }
-
+  mapToOperator() {
+    console.log('---- transformation Operator: mapTo ----');
+    let observable = from([11, 324, 2, 39, 34, 235]);
+    observable.pipe(mapTo(`helle ji`)).subscribe((res) => {
+      console.log(res);
+    });
+  }
   mergeMapOperator() {
     console.log('---- transformation Operator: mergeMap ----');
+    let observable1 = from([11, 324, 2, 39, 34, 235]);
+    let observable2 = from(['suraj', 'akash', 'shubham']);
   }
 }
